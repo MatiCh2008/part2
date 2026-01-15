0ba9208606931ac6e1c9d1b6da2d7e15ab397d04
MatiCh2008 <mateochmiel2008@gmail.com>
2026-01-15 13:08:02 +0100

    usunięcie starych komponentów z innych zadań

--- part2/src/components/Filter.jsx
+++
@@ -1,5 +0,0 @@
-const Filter = ({ term, handleTermChange }) => {
-    return <div>filter shown with <input value={term} onChange={handleTermChange}/></div>
-  }
-  
-  export default Filter
--- part2/src/components/Persons.jsx
+++
@@ -1,9 +0,0 @@
-import Person from './Person'
-
-const Persons = ({ personsToShow }) => {
-    return <div>{personsToShow.map(person =>
-        <Person key={person.name} person={person} />
-      )}</div>
-  }
-  
-  export default Persons
