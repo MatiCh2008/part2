974102b4f464f14dde964ad31799c5b83b49e722
MatiCh2008 <mateochmiel2008@gmail.com>
2026-01-15 13:08:50 +0100

    początek

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,88 +1,22 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-
-// test
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')  
-  const [term, setTerm] = useState('')  
-
-
-  
-  const addPerson = (event) => {
-
-    event.preventDefault()
-
-    const trimmedName = newName.trim().replace(/\s+/g, ' ');
-    const trimmedNumber = newNumber.trim().replace(/\s+/g, ' ');
-    
-    const personObject = {
-      name: trimmedName,
-      number: trimmedNumber
-    }
-
-    const nameExists = persons.some(person => person.name.toLowerCase() === trimmedName.toLowerCase())
-    const numberExists = persons.some(person => person.number === trimmedNumber)    
-
-    if (!trimmedName || !trimmedNumber){
-    alert(`Name and number are required`)
-    }    
-    else if (nameExists){
-    alert(`${trimmedName} is already added to phonebook`)
-    }
-    else if (numberExists){
-    alert(`Number ${trimmedNumber} is already added to phonebook`)      
-    }
-    else {
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')    
-    }
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }  
-
-  const handleTermChange = (event) => {
-    setTerm(event.target.value)
-  }    
-
-  console.log(persons)
-
-  const personsToShow = !term
-  ? persons
-  : persons.filter(person => person.name.toLowerCase().includes(term.toLowerCase()))
-
+const Hello = (props) => {
   return (
     <div>
-      <h2>Phonebook</h2>
-        <Filter term={term} handleTermChange={handleTermChange}/>
-      <h3>Add a new</h3>      
-        <PersonForm 
-          addPerson={addPerson} 
-          newName={newName} 
-          handleNameChange={handleNameChange}
-          newNumber={newNumber} 
-          handleNumberChange={handleNumberChange}/>
-      <h3>Numbers</h3>
-        <Persons personsToShow={personsToShow}/>
-        
-
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
-export default App
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
