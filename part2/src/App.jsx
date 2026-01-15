54df9eb4c50d82344abbf60e6bbd507c94fef8ad
MatiCh2008 <mateochmiel2008@gmail.com>
2026-01-15 12:42:25 +0100

    tt

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,89 +1,59 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-
-// test
+import Course from './components/Course'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')  
-  const [term, setTerm] = useState('')  
-
-// test 2
-
-  const addPerson = (event) => {
-
-    event.preventDefault()
-
-    const trimmedName = newName.trim().replace(/\s+/g, ' ');
-    const trimmedNumber = newNumber.trim().replace(/\s+/g, ' ');
-    
-    const personObject = {
-      name: trimmedName,
-      number: trimmedNumber
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
-
-    const nameExists = persons.some(person => person.name.toLowerCase() === trimmedName.toLowerCase())
-    const numberExists = persons.some(person => person.number === trimmedNumber)    
-
-    if (!trimmedName || !trimmedNumber){
-    alert(`Name and number are required`)
-    }    
-    else if (nameExists){
-    alert(`${trimmedName} is already added to phonebook`)
-    }
-    else if (numberExists){
-    alert(`Number ${trimmedNumber} is already added to phonebook`)      
-    }
-    else {
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')    
-    }
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }  
-
-  const handleTermChange = (event) => {
-    setTerm(event.target.value)
-  }    
-
-  console.log(persons)
-
-  const personsToShow = !term
-  ? persons
-  : persons.filter(person => person.name.toLowerCase().includes(term.toLowerCase()))
+  ]
 
   return (
     <div>
-      <h2>Phonebook</h2>
-        <Filter term={term} handleTermChange={handleTermChange}/>
-      <h3>Add a new</h3>      
-        <PersonForm 
-          addPerson={addPerson} 
-          newName={newName} 
-          handleNameChange={handleNameChange}
-          newNumber={newNumber} 
-          handleNumberChange={handleNumberChange}/>
-      <h3>Numbers</h3>
-        <Persons personsToShow={personsToShow}/>
-        
-
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}       
     </div>
   )
+
 }
 
 export default App
