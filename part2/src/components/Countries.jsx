02c9728e0012aac7e110d6dbc43d794316296fab
MatiCh2008 <mateochmiel2008@gmail.com>
2026-02-27 13:04:23 +0100

    zad 2.18 zakonczone

--- part2/src/components/Countries.jsx
+++ part2/src/components/Countries.jsx
@@ -1,21 +1,26 @@
 import Country from './Country'
+import CountryDetails from './CountryDetails';
 
 const Countries = ({ countries, term, handleShow }) => {
 
+    if (term === ""){
+        return null;        
+    }
+
     if (countries.length > 10){
         return <p>Too many matches, specify another filter</p>;
-    }        
-
-    if (countries.length === 0){
-            const country = countries[0]
+    }   
+    
+    if (countries.length === 1){
         return (
-            <Country 
-            key={country.cca3} 
-            country={country} 
-            handleShow={() => handleShow(country)} 
-            />            
+        <div>
+            <CountryDetails
+                country={countries[0]}
+                />
+
+        </div>            
         )
-    }        
+    }
 
     return (
         <div>
--- part2/src/components/CountryDetails.jsx
+++ part2/src/components/CountryDetails.jsx
@@ -1,10 +1,24 @@
 const CountryDetails = ({ country }) => {
+  const languages = country.languages ? Object.values(country.languages) : []
+
   return (
     <div>
       <div>
         <h3>{country.name.common}</h3>
-        <p>Capital: {country.capital}</p>
+        <p>Capital: {country.capital ? country.capital.join(', ') : 'No capital'}</p>
         <p>Area: {country.area}</p>
+        <h3>Languages</h3>
+        <ul>    
+          {languages.map(language =>
+           <li key={language}>{language}</li>
+          )}      
+        </ul> 
+
+        <img
+          src={country.flags.png}
+          alt={`flag of ${country.name.common}`}
+        />
+
       </div>
     </div>
   )
